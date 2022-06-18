ba60d9f53da96e8915f6b82c2e023b44cd525d99
jujunjuna189 <53271616+jujunjuna189@users.noreply.github.com>
2022-06-18 15:42:25 +0700

    Update

--- app/Http/Controllers/ApiController/WorkspaceController.php
+++ app/Http/Controllers/ApiController/WorkspaceController.php
@@ -10,22 +10,70 @@ class WorkspaceController extends Controller
 {
     public function create(Request $request)
     {
-        $dataClient = $request->data;
+        try {
+            $data['title'] = $request->title;
+            $data['description'] = $request->description;
+            $data['visibility'] = $request->visibility;
 
-        $data['title'] = $dataClient->title;
-        $data['description'] = $dataClient->description;
-        $data['visibility'] = $dataClient->visibility;
+            $create = WorkspaceModel::create($data);
 
-        WorkspaceModel::create($data);
-
-        return response()->json([
-            'status' => 'Success',
-            'data' => [],
-        ], 200);
+            if($create){
+                return response()->json([
+                    'status' => 'Success',
+                    'data' => $create,
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => 'Failed',
+                    'data' => [
+                        'Failed Create Data'
+                    ],
+                ], 300);
+            }   
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'Failed',
+                'data' => [
+                    'Server error'
+                ],
+            ], 500);
+        }
     }
 
     public function update(Request $request)
     {
+        try {
+            $dataRequest = $request;
+            $id = $dataRequest->id;
+            
+            $data['title'] = $dataRequest->title;
+            $data['description'] = $dataRequest->description;
+            $data['visibility'] = $dataRequest->visibility;
+
+            $result = WorkspaceModel::find($id);
+            $update = $result->update($data);
+
+            if($update){
+                return response()->json([
+                    'status' => 'Success',
+                    'data' => $update,
+                ], 200);
+            }else{
+                return response()->json([
+                    'status' => 'Failed',
+                    'data' => [
+                        'Failed Update Data'
+                    ],
+                ], 300);
+            }   
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'Failed',
+                'data' => [
+                    'Server error'
+                ],
+            ], 500);
+        }
     }
 
     public function delete(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Workspace
+Route::post('/workspace/create', [App\Http\Controllers\ApiController\WorkspaceController::class, 'create']);
+Route::post('/workspace/update', [App\Http\Controllers\ApiController\WorkspaceController::class, 'update']);
+Route::post('/workspace/delete', [App\Http\Controllers\ApiController\WorkspaceController::class, 'delete']);
+// Project
+Route::post('/project/create', [App\Http\Controllers\ApiController\ProjectController::class, 'create']);
+Route::post('/project/update', [App\Http\Controllers\ApiController\ProjectController::class, 'update']);
+Route::post('/project/delete', [App\Http\Controllers\ApiController\ProjectController::class, 'delete']);
+// Task
+Route::post('/task/create', [App\Http\Controllers\ApiController\TaskController::class, 'create']);
+Route::post('/task/update', [App\Http\Controllers\ApiController\TaskController::class, 'update']);
+Route::post('/task/delete', [App\Http\Controllers\ApiController\TaskController::class, 'delete']);
